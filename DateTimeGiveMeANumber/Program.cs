4470bcf810b5253f3ea4f33242eb8aa95549d50f
LaurieSue <pink.bee.me@outlook.com>
2020-11-16 08:41:23 -0600

    Add project files.

---
+++ DateTimeGiveMeANumber/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeGiveMeANumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now;
+            Console.WriteLine("The current date and time is now: " + now);
+            Console.WriteLine("\nHow many hours will it be until you must call the taxi to take you to the airport? ");
+            int hoursTilDeparture = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nGreat!! The taxi service will be called by : {0}\nHave A Nice Trip!!!", now.AddHours(hoursTilDeparture));
+            Console.ReadLine();
+        }
+    }
+}
